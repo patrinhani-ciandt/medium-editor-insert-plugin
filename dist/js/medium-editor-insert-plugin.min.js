eb4c3b48f379185562bad6d7d61c5f80211806fd
Vinicius Patrinhani <patrinhani@ciandt.com>
2015-12-18 08:26:13 -0200

    Removed unnecessary extra top margin on medium-insert-buttons-show style

--- dist/js/medium-editor-insert-plugin.min.js
+++ dist/js/medium-editor-insert-plugin.min.js
@@ -1,5 +1,5 @@
 /*! 
- * medium-editor-insert-plugin v2.100.11 - jQuery insert plugin for MediumEditor
+ * medium-editor-insert-plugin v2.100.12 - jQuery insert plugin for MediumEditor
  *
  * https://github.com/orthes/medium-editor-insert-plugin
  * 
